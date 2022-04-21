9ba3b5aa22ddd001e0d691c80051674d154f9452
DennisNguyen97 <89007787+DennisNguyen97@users.noreply.github.com>
2022-04-22 00:24:32 +0700

    Add files via upload
    create card

---
+++ config.js
@@ -0,0 +1,14 @@
+const nameGirl = "";
+const giftUrl = "http://nodemy.vn";
+const eventName = "Chúc Mừng Sinh Nhật Tiểu Thư";
+const titleCard = "Tặng Tiểu Thư";
+const contentCard =
+  "Chúc mọi điều đến với tiểu thư sẽ luôn là điều tuyệt vời nhất, chúc mỗi phút giây tiểu thư trải qua là những phút giây hạnh phúc, ngọtngào. Chúc cho tụi mình vẫn mãi bên cạnh nhau. Sinh nhật vui vẻ nha tiểu thư.";
+
+// phần dưới dành cho các bạn biết code, nếu muốn chỉnh ảnh đơn giản với base64
+// Cần hỗ trợ hãy liên hệ:
+// Mr-Nam http://facebook.com/nam.nodemy
+// Các bạn muốn học lập trình thì tham gia Nhóm zalo tự học lập trình nhé: https://zalo.me/g/yhdkef092
+const giftImage = "tieu-thu.jpg";
+const base64 = "";
+const giftImageBase64 = "data:image/jpg;base64, " + base64;
